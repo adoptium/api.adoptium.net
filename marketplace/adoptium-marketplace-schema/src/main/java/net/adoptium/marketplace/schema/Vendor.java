c0b6e4e7cd01f826e00a3c9ac193ac2b2f4fe81c
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-02-03 15:28:17 +0000

    Update Vendor, remove adoptopenjdk, openjdk->redhat

--- marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
+++ marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
@@ -6,5 +6,5 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
     Use a schema ref as adopt and adoptium will have a different subset of vendors
 */
 public enum Vendor {
-    adoptium, adoptopenjdk, openjdk, alibaba, ibm;
+    adoptium, redhat, alibaba, ibm;
 }
