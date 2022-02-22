e76938360fc1ba2707088a0810af5581eb370f4b
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-02-22 00:08:53 +0000

    Add microsoft vendor

--- marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
+++ marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
@@ -4,5 +4,5 @@ package net.adoptium.marketplace.schema;
     Use a schema ref as adopt and adoptium will have a different subset of vendors
 */
 public enum Vendor {
-    adoptium, redhat, alibaba, ibm;
+    adoptium, redhat, alibaba, ibm, microsoft;
 }
