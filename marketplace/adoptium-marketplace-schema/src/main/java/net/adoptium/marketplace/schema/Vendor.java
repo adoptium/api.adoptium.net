23809db4ad4e2a7a12d2e9d25e0d1a3dbad5d6d8
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-02-22 00:30:03 +0000

    Add microsoft distribution

--- marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
+++ marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Vendor.java
@@ -1,8 +1,12 @@
 package net.adoptium.marketplace.schema;
 
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 /*
     Use a schema ref as adopt and adoptium will have a different subset of vendors
 */
+@Schema(type = SchemaType.STRING, enumeration = {"adoptium", "redhat", "alibaba", "ibm", "microsoft"})
 public enum Vendor {
     adoptium, redhat, alibaba, ibm, microsoft;
 }
