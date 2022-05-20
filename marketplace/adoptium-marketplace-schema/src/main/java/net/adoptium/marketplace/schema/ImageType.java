f14e54fe723b5c94b3fa401e70e12c1b4429dcf0
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-05-20 12:13:26 +0100

    Add semeru and remove extra Image types

--- marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/ImageType.java
+++ marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/ImageType.java
@@ -3,12 +3,8 @@ package net.adoptium.marketplace.schema;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-@Schema(type = SchemaType.STRING, enumeration = {"jdk", "jre", "testimage", "debugimage", "staticlibs", "sources"}, example = "jdk")
+@Schema(type = SchemaType.STRING, enumeration = {"jdk", "jre"}, example = "jdk")
 public enum ImageType {
     jdk,
-    jre,
-    testimage,
-    debugimage,
-    staticlibs,
-    sources;
+    jre;
 }
