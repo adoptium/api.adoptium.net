8e261039342305bd7c3b898b37cbe1a271523df2
John Oliver <1615532+johnoliver@users.noreply.github.com>
2022-05-23 16:59:41 +0100

    Add redhat Distribution

--- marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Distribution.java
+++ marketplace/adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Distribution.java
@@ -3,7 +3,7 @@ package net.adoptium.marketplace.schema;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-@Schema(type = SchemaType.STRING, enumeration = {"temurin", "dragonwell", "zulu", "microsoft", "semeru", "bisheng"})
+@Schema(type = SchemaType.STRING, enumeration = {"temurin", "dragonwell", "zulu", "microsoft", "semeru", "bisheng", "redhat"})
 public enum Distribution {
-    temurin, dragonwell, zulu, microsoft, semeru, bisheng;
+    temurin, dragonwell, zulu, microsoft, semeru, bisheng, redhat;
 }
