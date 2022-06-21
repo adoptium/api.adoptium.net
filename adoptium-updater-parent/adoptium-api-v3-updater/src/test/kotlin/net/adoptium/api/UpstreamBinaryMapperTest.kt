fed6044a279ad45a827a1570ffb37a7ac978e3e1
Wen Zhou <wenzhou@redhat.com>
2022-06-21 20:36:24 +0200

    test: remove test case on sbom in upstream
    	- since we do not add sbom to upstream no need to have a
    testcase

--- adoptium-updater-parent/adoptium-api-v3-updater/src/test/kotlin/net/adoptium/api/UpstreamBinaryMapperTest.kt
+++ adoptium-updater-parent/adoptium-api-v3-updater/src/test/kotlin/net/adoptium/api/UpstreamBinaryMapperTest.kt
@@ -62,8 +62,7 @@ class UpstreamBinaryMapperTest : BaseTest() {
                 "OpenJDK11U-jdk_x64_linux_11.0.10_3_ea.tar.gz",
                 "OpenJDK11U-jre-shenandoah_x64_linux_11.0.10_3_ea.tar.gz",
                 "OpenJDK11U-jdk-shenandoah_x64_linux_11.0.10_3_ea.tar.gz",
-                "OpenJDK11U-testimage-shenandoah_x64_linux_11.0.10_3_ea.tar.gz",
-                "OpenJDK18U-sbom_aarch64_alpine-linux_hotspot_2022-05-27-23-30.tar.gz"
+                "OpenJDK11U-testimage-shenandoah_x64_linux_11.0.10_3_ea.tar.gz"
             )
         )
 
@@ -80,7 +79,6 @@ class UpstreamBinaryMapperTest : BaseTest() {
             assertEquals(ImageType.jre, binaryList[6].image_type)
             assertEquals(ImageType.jdk, binaryList[7].image_type)
             assertEquals(ImageType.testimage, binaryList[8].image_type)
-            assertEquals(ImageType.sbom, binaryList[9].image_type)
         }
     }
 
