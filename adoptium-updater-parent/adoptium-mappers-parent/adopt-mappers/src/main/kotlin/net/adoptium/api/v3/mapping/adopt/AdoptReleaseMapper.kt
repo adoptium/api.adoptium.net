26d3aae4197a3e6b0bfbd96aa3eaff2845203614
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-05-25 03:05:29 +0100

    Add aqavit tap files (#568)

--- adoptium-updater-parent/adoptium-mappers-parent/adopt-mappers/src/main/kotlin/net/adoptium/api/v3/mapping/adopt/AdoptReleaseMapper.kt
+++ adoptium-updater-parent/adoptium-mappers-parent/adopt-mappers/src/main/kotlin/net/adoptium/api/v3/mapping/adopt/AdoptReleaseMapper.kt
@@ -75,6 +75,7 @@ private class AdoptReleaseMapper constructor(
         val ghAssetsWithMetadata = associateMetadataWithBinaries(ghRelease.releaseAssets)
         val sourcePackage = getSourcePackage(ghRelease, ghAssetsWithMetadata)
         val releaseNotes = getReleaseNotesPackage(ghRelease.releaseAssets.assets)
+        val aqavitResultsLink = getAqavitLink(ghRelease.releaseAssets.assets)
 
         try {
             val ghAssetsGroupedByVersion = ghAssetsWithMetadata
@@ -91,7 +92,7 @@ private class AdoptReleaseMapper constructor(
                     val ghAssets: List<GHAsset> = ghAssetsForVersion.value.map { ghAssetWithMetadata -> ghAssetWithMetadata.key }
                     val id = generateIdForSplitRelease(version, ghRelease)
 
-                    toRelease(releaseName, ghAssets, ghAssetsWithMetadata, id, releaseType, releaseLink, timestamp, updatedAt, vendor, version, ghRelease.releaseAssets.assets, sourcePackage, releaseNotes)
+                    toRelease(releaseName, ghAssets, ghAssetsWithMetadata, id, releaseType, releaseLink, timestamp, updatedAt, vendor, version, ghRelease.releaseAssets.assets, sourcePackage, releaseNotes, aqavitResultsLink)
                 }
                 .ifEmpty {
                     try {
@@ -100,7 +101,7 @@ private class AdoptReleaseMapper constructor(
                         val ghAssets = ghRelease.releaseAssets.assets
                         val id = ghRelease.id.id
 
-                        return@ifEmpty listOf(toRelease(releaseName, ghAssets, ghAssetsWithMetadata, id, releaseType, releaseLink, timestamp, updatedAt, vendor, version, ghAssets, sourcePackage, releaseNotes))
+                        return@ifEmpty listOf(toRelease(releaseName, ghAssets, ghAssetsWithMetadata, id, releaseType, releaseLink, timestamp, updatedAt, vendor, version, ghAssets, sourcePackage, releaseNotes, aqavitResultsLink))
                     } catch (e: Exception) {
                         throw FailedToParse("Failed to parse version $releaseName", e)
                     }
@@ -113,6 +114,19 @@ private class AdoptReleaseMapper constructor(
             LOGGER.error("Failed to parse $releaseName")
             return ReleaseResult(error = "Failed to parse $releaseName")
         }
+    }
+
+    private fun getAqavitLink(assets: List<GHAsset>): String? {
+        val aqavitAssets = assets
+            .filter { it.name.contains("AQAvitTapFiles") }
+            .map { it.downloadUrl }
+            .toList()
+
+        if (aqavitAssets.size > 1) {
+            LOGGER.warn("Multiple AqaVit assets present on release")
+        }
+
+        return aqavitAssets.firstOrNull()
     }
 
     private fun getReleaseVersion(ghAssetWithMetadata: Map.Entry<GHAsset, GHMetaData>): String {
@@ -169,7 +183,8 @@ private class AdoptReleaseMapper constructor(
         version: VersionData,
         fullGhAssetList: List<GHAsset>,
         sourcePackage: SourcePackage?,
-        releaseNotes: ReleaseNotesPackage?
+        releaseNotes: ReleaseNotesPackage?,
+        aqavitResultsLink: String?
     ): Release {
         LOGGER.debug("Getting binaries $releaseName")
         val binaries = adoptBinaryMapper.toBinaryList(ghAssets, fullGhAssetList, ghAssetWithMetadata)
@@ -181,7 +196,21 @@ private class AdoptReleaseMapper constructor(
             }
             .map { it.downloadCount }.sum()
 
-        return Release(id, release_type, releaseLink, releaseName, DateTime(timestamp), DateTime(updatedAt), binaries.toTypedArray(), downloadCount, vendor, version, sourcePackage, releaseNotes)
+        return Release(
+            id,
+            release_type,
+            releaseLink,
+            releaseName,
+            DateTime(timestamp),
+            DateTime(updatedAt),
+            binaries.toTypedArray(),
+            downloadCount,
+            vendor,
+            version,
+            sourcePackage,
+            releaseNotes,
+            aqavitResultsLink
+        )
     }
 
     private fun formReleaseType(release: GHRelease): ReleaseType {
