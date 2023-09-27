6ecfa1b40b937071d75d5c1e76a0b355a2583581
George Adams <georgeadams1995@gmail.com>
2023-09-27 13:23:51 +0100

    add pod name to server headers

--- adoptium-frontend-parent/adoptium-api-v3-frontend/src/main/kotlin/net/adoptium/api/v3/metrics/OtelInterceptor.kt
+++ adoptium-frontend-parent/adoptium-api-v3-frontend/src/main/kotlin/net/adoptium/api/v3/metrics/OtelInterceptor.kt
@@ -8,13 +8,21 @@ import jakarta.ws.rs.ext.Provider
 
 @Provider
 class AfterContainerFilter : ContainerResponseFilter {
+    // Fetch the hostname once and cache it
+    private val hostname: String = System.getenv("HOSTNAME") ?: "unknown"
+
     override fun filter(requestContext: ContainerRequestContext?, responseContext: ContainerResponseContext?) {
         try {
+            // Existing User-Agent code
             val userAgent = requestContext?.headers?.get("User-Agent")?.getOrNull(0)
             val span: Span? = Span.current()
             if (userAgent != null && span != null) {
                 span.setAttribute("User-Agent", userAgent)
             }
+
+            // Add the pod's hostname as a response header
+            responseContext?.headers?.add("X-Pod-Hostname", hostname)
+
         } catch (_: Exception) {
             //Ignore
         }
