89179a411ca474cc333a81284325f7ade72b23b9
Xavier FACQ <xavier.facq@free.fr>
2024-01-16 21:49:35 +0100

    Add tests

---
+++ adoptium-frontend-parent/adoptium-api-v3-frontend/src/test/kotlin/net/adoptium/api/packages/ChecksumPathTest.kt
@@ -0,0 +1,36 @@
+package net.adoptium.api.packages
+
+import io.quarkus.test.junit.QuarkusTest
+import net.adoptium.api.DbExtension
+import net.adoptium.api.v3.models.Architecture
+import net.adoptium.api.v3.models.HeapSize
+import net.adoptium.api.v3.models.ImageType
+import net.adoptium.api.v3.models.JvmImpl
+import net.adoptium.api.v3.models.OperatingSystem
+import net.adoptium.api.v3.models.Project
+import net.adoptium.api.v3.models.Vendor
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@QuarkusTest
+@ExtendWith(value = [DbExtension::class])
+class ChecksumPathTest : PackageEndpointTest() {
+    override fun getPath(): String {
+        return "/v3/checksum"
+    }
+
+    @Test
+    fun versionRequestRedirects() {
+        requestChecksumExpecting307(::getRandomBinary) { release, binary ->
+            getVersionPath(release.release_name, binary.os, binary.architecture, binary.image_type, binary.jvm_impl, binary.heap_size, release.vendor, binary.project)
+        }
+    }
+
+    @Test
+    fun nonExistantVersionRequestGives404() {
+        val path = getVersionPath("fooBar", OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.jdk)
+        performRequest(path)
+            .then()
+            .statusCode(404)
+    }
+}
