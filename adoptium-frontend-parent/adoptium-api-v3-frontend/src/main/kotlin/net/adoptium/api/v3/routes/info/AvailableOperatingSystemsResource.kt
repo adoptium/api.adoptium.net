14991a7bc11cf67a1eb7029d37688167874fff1a
Xavier FACQ <xavier.facq@free.fr>
2024-04-20 14:12:05 +0200

    remove unused imports

--- adoptium-frontend-parent/adoptium-api-v3-frontend/src/main/kotlin/net/adoptium/api/v3/routes/info/AvailableOperatingSystemsResource.kt
+++ adoptium-frontend-parent/adoptium-api-v3-frontend/src/main/kotlin/net/adoptium/api/v3/routes/info/AvailableOperatingSystemsResource.kt
@@ -5,8 +5,6 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import jakarta.ws.rs.core.Response
-import jakarta.ws.rs.core.UriInfo
 import net.adoptium.api.v3.models.OperatingSystem
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
