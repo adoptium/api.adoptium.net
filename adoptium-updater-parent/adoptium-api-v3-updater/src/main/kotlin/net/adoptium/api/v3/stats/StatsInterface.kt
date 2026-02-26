0c713dd3584cb20963d8a2455b7d40d34530443d
jdvr <juandavidvegarguez@gmail.com>
2026-02-26 22:20:36 +0000

    ensure all update run

--- adoptium-updater-parent/adoptium-api-v3-updater/src/main/kotlin/net/adoptium/api/v3/stats/StatsInterface.kt
+++ adoptium-updater-parent/adoptium-api-v3-updater/src/main/kotlin/net/adoptium/api/v3/stats/StatsInterface.kt
@@ -5,6 +5,7 @@ import jakarta.inject.Inject
 import net.adoptium.api.v3.dataSources.models.AdoptRepos
 import net.adoptium.api.v3.stats.cloudflare.CloudflareStatsCalculator
 import net.adoptium.api.v3.stats.dockerstats.DockerStatsInterfaceFactory
+import org.slf4j.LoggerFactory
 
 @ApplicationScoped
 open class StatsInterface @Inject constructor(
@@ -12,11 +13,23 @@ open class StatsInterface @Inject constructor(
     private val cloudFlareStatsCalculator: CloudflareStatsCalculator,
     dockerStatsInterfaceFactory: DockerStatsInterfaceFactory
 ) {
+    companion object {
+        @JvmStatic
+        private val LOGGER = LoggerFactory.getLogger(this::class.java)
+    }
+
     private val dockerStats = dockerStatsInterfaceFactory.get()
+
     open suspend fun update(repos: AdoptRepos) {
-        gitHubDownloadStatsCalculator.saveStats(repos)
-        dockerStats.updateDb()
-        cloudFlareStatsCalculator.updateDb()
+        handlingUpdateException("github") { gitHubDownloadStatsCalculator.saveStats(repos) }
+        handlingUpdateException("docker") { dockerStats.updateDb() }
+        handlingUpdateException("cloudflare") { cloudFlareStatsCalculator.updateDb() }
+    }
+
+    private suspend fun handlingUpdateException(source: String, fn: suspend () -> Unit) = try {
+        fn()
+    } catch (e: Exception) {
+        LOGGER.error("Failed to update $source, continuing with other sources", e)
     }
 
     open suspend fun updateStats() {}
